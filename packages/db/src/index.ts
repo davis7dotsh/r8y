ef35933aa9658f663a19d4cff323e33ff6a8de94
Ben Davis <davis.benjamin41902@gmail.com>
2025-10-31 17:18:42 -0700

    prettier

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,9 +4,9 @@ export * from './connection';
 import type * as DB_SCHEMA from './schema';
 
 export type DBTypes = {
-    [K in keyof typeof DB_SCHEMA]: (typeof DB_SCHEMA)[K] extends {
-      $inferSelect: unknown;
-    }
-      ? (typeof DB_SCHEMA)[K]["$inferSelect"]
-      : never;
-  };
+	[K in keyof typeof DB_SCHEMA]: (typeof DB_SCHEMA)[K] extends {
+		$inferSelect: unknown;
+	}
+		? (typeof DB_SCHEMA)[K]['$inferSelect']
+		: never;
+};
